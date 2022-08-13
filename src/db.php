4acab0c2e6fbb78e1d138c5399c3f616a6cc4623
dreammaker97 <87482298+dreammaker97@users.noreply.github.com>
2022-08-14 00:57:49 +0700

    Delete src directory

--- src/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// error_reporting(0);
-// ini_set('display_errors', 0);
-$servername = "localhost";
-$username = "id17855911_vieon";
-$password = "Tuananh@1997";
-$database = "id17855911_dbvieon";
-$conn = new mysqli($servername, $username, $password, $database);
-?>
